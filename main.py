d3744bb56c2ab12e1746fd8f0cbec51c6d203d0f
fox-io <gitlab.fox-io@foxdata.io>
2021-07-28 21:59:30 -0500

    Add a json data file to which we save new entries (plaintext).

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ tk-password-generator
 (c)2021 John Mann <github.fox-io@foxdata.io>
 """
 import tkinter
+import json
 
 
 class PasswordGenerator:
@@ -26,6 +27,7 @@ class PasswordGenerator:
 
         self.website_entry = tkinter.Entry(width=51)
         self.website_entry.grid(column=1, row=1, columnspan=2, sticky=tkinter.W)
+        self.website_entry.focus()
         self.username_entry = tkinter.Entry(width=51)
         self.username_entry.grid(column=1, row=2, columnspan=2, sticky=tkinter.W)
         self.password_entry = tkinter.Entry(width=32)
@@ -34,8 +36,31 @@ class PasswordGenerator:
         self.generate_button = tkinter.Button(text="Generate Password")
         self.generate_button.grid(column=2, row=3, sticky=tkinter.W)
 
-        self.add_button = tkinter.Button(text="Add", width=43)
+        self.add_button = tkinter.Button(text="Add", width=43, command=self.add_entry)
         self.add_button.grid(column=1, row=4, columnspan=2, sticky=tkinter.W)
+
+    def add_entry(self):
+        # Create a dict using input data
+        form_data = {
+            "website": self.website_entry.get(),
+            "username": self.username_entry.get(),
+            "password": self.password_entry.get()
+        }
+
+        with open(file="./data.json", mode="r+") as data_file:
+            # Read the current file contents
+            file_data = json.load(data_file)
+            # Add new data with an index at the end of the json
+            entry_count = len(file_data)
+            file_data[f"{entry_count}"] = form_data
+            # Move file pointer back to beginning and write json data
+            data_file.seek(0)
+            json.dump(file_data, data_file, indent=4)
+
+        # Clear form
+        self.website_entry.delete(0, tkinter.END)
+        self.username_entry.delete(0, tkinter.END)
+        self.password_entry.delete(0, tkinter.END)
 
 
 def main():
